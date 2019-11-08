1f3d8a0f0806939fadb0432dfe80293a001d70e8
Cayo <cayocan@hotmail.com>
2019-11-07 22:30:30 -0200

    percurso

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import FirstComponent from "./components/learning-examples/FirstComponent";
-import SecondComponent from "./components/learning-examples/SecondComponent";
-import ThirdComponent from "./components/learning-examples/ThirdComponent";
-import FourthComponent from "./components/learning-examples/FourthComponent";
-import Counter from './components/counter/Counter';
+import Counter from "./components/counter/Counter";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">			
-		    <Counter/>                
-            </header>
+            <Counter />
         </div>
     );
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,39 +1,74 @@
 import React, { Component } from "react";
 import "./Counter.css";
+import PropTypes from "prop-types";
 
 //Default export component class from FirstComponent.jsx Module
 export default class Counter extends Component {
+    render() {
+        return (
+            <div className="counter">
+                <CounterButton by={1} incrementMethod={this.increment} />
+                <CounterButton by={5} incrementMethod={this.increment} />
+                <CounterButton by={10} incrementMethod={this.increment} />
+            </div>
+        );
+    }
+
+    increment(by) {
+        //console.log(`Increment from parent - ${by}`);
+        this.setState({
+            counter: this.state.counter + by
+        });
+    }
+}
+
+class CounterButton extends Component {
     //Define the initial state in a constructor
     //state => counter = 0
     constructor() {
         super();
+
         this.state = {
             counter: 0
         };
 
+        /*
+            We can not use binding when we
+            transform normal functions in
+            arrow functions: 
+            render = () => {};
+            increment = () => {};
+            decrement = () =>{};
+        */
         this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
+        //this.decrement = this.decrement.bind(this);
     }
 
     render() {
         return (
             <div className="Counter">
-                <button onClick={this.increment}>+1</button>
+                <button onClick={this.increment}>+{this.props.by}</button>
                 <span className="count">{this.state.counter}</span>
-                <button onClick={this.decrement}>-1</button>
+                <button onClick={this.decrement}>-{this.props.by}</button>
             </div>
         );
     }
 
-    increment() {
+    increment(by) {
         //Update state - counter++
-        this.state.counter++;
-        this.setState({ counter: this.state.counter++ });
+        this.setState({
+            counter: this.state.counter + this.props.by
+        });
+
+        this.props.incrementMethod(this.props.by);
     }
 
-    decrement() {
-        //Update state - counter--
-        this.state.counter--;
-        this.setState({ counter: this.state.counter-- });
-    }
 }
+
+CounterButton.defaultProps = {
+    by: 1
+};
+
+CounterButton.propTypes = {
+    by: PropTypes.number
+};
